d428ad82f4c701bba881cc6207671f0f5bfd3093
Alican <alicanhaytaoglu3407@gmail.com>
2024-03-11 00:26:33 +0300

    skor yazısı eklendi font ve konumu ayarlandı

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import turtle
 
-turtle_board=turtle.Screen()
-turtle_board.bgcolor("green")
-turtle_board.title("Turtle Game ")
-def kronometre():
-    for i in range(50):
-        print(i)
+#Screen
+drawing_board=turtle.Screen()
+drawing_board.bgcolor("green")
+drawing_board.title("Turtle Game ")
+FONT=('Arial',30,'normal')
+
+#score Turtle
+score_turtle=turtle.Turtle()
+score_turtle.hideturtle()# imleci gizledi
+score_turtle.color("dark blue")
+score_turtle.penup()
+top_height=drawing_board.window_height()/2#ekranın yarasından ölçecez çünkü yarısında başlatıyor
+y=top_height*0.8#  burada y  açılan ekranın aşağıdan yukarıya doğru yüzde 80 ninde dursun diyoruz
+score_turtle.setposition(0,y) # burada yazının kordinat veriyoruz
+score_turtle.write(arg="Score 0 :",move=False,align='Center',font=FONT)
+
+
+
+
 turtle.mainloop()
